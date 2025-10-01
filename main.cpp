bb75fae7d5f2170e6f512412a11f212a8e1538aa
Krish Mathur <krishmathur21@gmail.com>
2025-10-01 21:31:47 +0530

    adding new file

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a, b;
+    cout << "enter 2 numbers : " << endl;
+    cin >> a >> b;
+    int sum = a + b;
+
+    cout << sum;
+
+}
